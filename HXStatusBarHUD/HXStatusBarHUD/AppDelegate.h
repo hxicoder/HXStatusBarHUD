18c223313b51eaeac7ff7a9f4946b8ee2e4674e2
Huang Xing <huangxing@huangdemacbook-pro.local>
2016-04-02 17:33:45 +0800

    初始化项目

---
+++ HXStatusBarHUD/HXStatusBarHUD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HXStatusBarHUD
+//
+//  Created by Huang Xing on 16/4/2.
+//  Copyright © 2016年 Huang Xing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HXStatusBarHUD/HXStatusBarHUD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HXStatusBarHUD
+//
+//  Created by Huang Xing on 16/4/2.
+//  Copyright © 2016年 Huang Xing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
